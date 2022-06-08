a082edc244f9016413ca9ecdb0317b0541211c5a
lauratro <lauratronchin@hotmail.it>
2022-06-08 10:20:38 +0200

    Add variable to set inputs values

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Login() {
+  const[username,setUsername]=useState("karl.kroeber@thekey.technology")
+  const[passwort,setPasswort]=useState("testtest")
   return (
     <div>
         <form>
 <div>
   <label htmlFor='username'>Username</label>
-  <input type="text"  />
+  <input type="text" value={username}  />
 </div>
 <div>
   <label htmlFor='passwort'>Passwort</label>
-  <input type="text"  />
+  <input type="text" value={passwort}  />
 </div>
 <button type="submit">Log in</button>
         </form>
