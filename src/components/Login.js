97be718ab44f846155d7624f5b8b602ac24b4e2c
lauratro <lauratronchin@hotmail.it>
2022-06-08 10:18:20 +0200

    Create form structure

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <form>
+<div>
+  <label htmlFor='username'>Username</label>
+  <input type="text"  />
+</div>
+<div>
+  <label htmlFor='passwort'>Passwort</label>
+  <input type="text"  />
+</div>
+<button type="submit">Log in</button>
+        </form>
+    </div>
   )
 }
