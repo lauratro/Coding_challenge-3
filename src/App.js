8c5af2bb128c99c1d6f3a539a0d2b80198fe0d43
lauratro <lauratronchin@hotmail.it>
2022-06-08 10:34:54 +0200

    Add apollo client provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Login from "./components/Login"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "https://staging.api.wissenwirkt.com/api/graphql",
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
     <div className="App">
+       <ApolloProvider client={client}>
       <Login />
+      </ApolloProvider>
     </div>
   );
 }
