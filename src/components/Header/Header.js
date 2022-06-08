083c0aec94402e93181297825606ce1ee0d2f8e7
lauratro <lauratronchin@hotmail.it>
2022-06-08 20:30:29 +0200

    Create header and add style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div class="headerCont">
+        <div className='companyName'>INFECTOPHARM</div>
+    </div>
+  )
+}
