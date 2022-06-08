098e40fd3e2a6a1d2053ec91777b61a0b3fc4107
lauratro <lauratronchin@hotmail.it>
2022-06-08 20:30:58 +0200

    Import CourseList and header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import Login from "./components/Login"
+import Login from "./components/Login/Login"
+import CourseList from './components/CourseList/CourseList';
+import Header from './components/Header/Header';
 
 function App() {
   return (
 
  
     <div className="App">
-     
+     <Header />
       <Login />
+      <CourseList />
      
     </div>
   );
