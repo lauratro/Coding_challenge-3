aa0c8edee522e1bdeff47b43b0f953634fa9ca2b
lauratro <lauratronchin@hotmail.it>
2022-06-08 20:31:37 +0200

    Add style to login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,66 @@
+import React,{useState} from 'react'
+import { useMutation, gql } from '@apollo/client'
+import "./Login.css"
+
+
+const CREATE_AUTH_MUTATION= gql`
+mutation Auth{
+  Auth {
+    login(input: { email: "karl.kroeber@thekey.technology", password:"testtest" }) {
+      token
+      userId
+     
+    }
+ 
+  }
+}
+`
+
+
+export default function Login() {
+  const[username,setUsername]=useState("karl.kroeber@thekey.technology")
+  const[passwort,setPasswort]=useState("testtest")
+  
+
+
+
+
+
+  const [createAuth, { data, loading, error }] = useMutation(CREATE_AUTH_MUTATION, {
+    onCompleted({Auth}) {
+      if (Auth) {
+        console.log(Auth.login.token)
+        localStorage.setItem('token', Auth.login.token );
+        localStorage.setItem('userId',Auth.login.userId);
+      
+      }
+    }
+  })
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
+
+console.log(data)
+
+  return (
+    <div>
+        <form  className='formContainer'  onSubmit={(e) => {
+        e.preventDefault();
+        createAuth();
+     
+      }}>
+<div className='inputField'>
+  <label className='labelStyle' htmlFor='username'>Username</label>
+  <input   type="text" defaultValue={"xxxxxxxx"}  />
+</div>
+<div className='inputField'>
+  <label className='labelStyle' htmlFor='passwort'>Passwort</label>
+  <input type="text" defaultValue={"xxxxxx"}  />
+</div>
+<div className='buttonCont'>
+<button className='submitButton' type="submit">Log in</button>
+</div>
+        </form>
+   
+    </div>
+  )
+}
